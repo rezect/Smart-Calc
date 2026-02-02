39c25c33512fcbf065d8f90abde2c8348e0b026a
rezect <ilyapr1568@yandex.ru>
2026-02-02 10:09:01 +0300

    feat: changed recursive method to standart Dijcstra with functions

--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -7,16 +7,15 @@ import (
 )
 
 var tokenTypePriority = map[string]int{
+	// Operators
 	"+": 1,
 	"-": 1,
 	"/": 2,
 	"*": 2,
-}
 
-var functionArgs = map[string]int{
-	"sin": 1,
-	"cos": 1,
-	"log": 2,
+	// Functions
+	"sin": 9,
+	"cos": 9,
 }
 
 func HandleEquation(equationString string) error {
@@ -26,84 +25,29 @@ func HandleEquation(equationString string) error {
 		panic(err)
 	}
 
+	// Преобразуем в RPN
+	tokensRPN, err := tokensToRPN(tokens)
+	if err != nil {
+		panic(err)
+	}
+
 	// Считаем значение выражения
-	result, err := calculateEquation(tokens)
+	result, err := calculateEquation(tokensRPN)
 	fmt.Printf("Результат: %v", result)
 
 	return nil
 }
 
-//5*8*cos(2+9)+(7*5+8-9*sin(5*5)+5)
+// 5*8*cos(2+9)+(7*5+8-9*sin(5*5)+5)
 func tokensToRPN(tokens []Token) ([]Token, error) {
 	var operators = []Token{}
 	var rpn = []Token{}
 
-	for i := 0; i < len(tokens); i++ {
-		token := tokens[i]
+	for _, token := range tokens {
 		if token.Type == Number {
 			rpn = append(rpn, token)
-		} else if token.Type == Function {
-			// Убеждаемся, что следующий токен существует и он - Lparen
-			if i >= len(tokens)-1 {
-				return nil, fmt.Errorf("После функции должны быть скобки")
-			} else if tokens[i+1].Type != Lparen {
-				return nil, fmt.Errorf("После функции должны быть скобки")
-			}
-
-			// Считываем токены до соотв. правой скобки
-			// TODO: более качественная обработка ошибок
-			var parameterTokens = []Token{}
-			var parameters = []float64{}
-
-			parenCounter := 0
-			funcToken := token
-			var funcResult float64 = 0
-
-			for j := i + 2; j < len(tokens); j++ {
-				token = tokens[j]
-				if token.Type == Comma {
-					argResult, err := calculateArgument(&parameterTokens, funcToken.Value)
-					if err != nil {
-						return nil, err
-					}
-
-					parameters = append(parameters, argResult)
-				} else {
-					if token.Type == Lparen {
-						parenCounter++
-					} else if token.Type == Rparen {
-						if parenCounter == 0 {
-							argResult, err := calculateArgument(&parameterTokens, funcToken.Value)
-							if err != nil {
-								return nil, err
-							}
-							
-							parameters = append(parameters, argResult)
-
-							switch funcToken.Value {
-							case "sin":
-								if len(parameters) != 1 {
-									return nil, fmt.Errorf("У синуса может быть только один параметр")
-								}
-								funcResult = math.Sin(parameters[0])
-							case "cos":
-								if len(parameters) != 1 {
-									return nil, fmt.Errorf("У косинуса может быть только один параметр")
-								}
-								funcResult = math.Cos(parameters[0])
-							}
-
-							i = j
-							break
-						} else {
-							parenCounter--
-						}
-					}
-					parameterTokens = append(parameterTokens, token)
-				}
-			}
-
-			rpn = append(rpn, Token{Number, strconv.FormatFloat(funcResult, 'f', -1, 64), 0})
+		} else if t := token.Type; t == Function || t == Lparen {
+			operators = append(operators, token)
 		} else if token.Type == Operator {
 			if len(operators) == 0 {
 				operators = append(operators, token)
@@ -123,8 +67,6 @@ func tokensToRPN(tokens []Token) ([]Token, error) {
 					operators = append(operators, token)
 				}
 			}
-		} else if token.Type == Lparen {
-			operators = append(operators, token)
 		} else if token.Type == Rparen {
 			if len(operators) == 0 {
 				return nil, fmt.Errorf("Неправильное расположение закрывающей скобки")
@@ -137,6 +79,8 @@ func tokensToRPN(tokens []Token) ([]Token, error) {
 				}
 			}
 			operators = operators[:len(operators)-1]
+		} else if token.Type == Comma {
+			continue
 		}
 	}
 	for len(operators) > 0 {
@@ -148,12 +92,8 @@ func tokensToRPN(tokens []Token) ([]Token, error) {
 
 func calculateEquation(tokens []Token) (float64, error) {
 	var stack = []float64{}
-	tokensRPN, err := tokensToRPN(tokens)
-	if err != nil {
-		return 0, err
-	}
 
-	for _, token := range tokensRPN {
+	for _, token := range tokens {
 		if token.Type == Number {
 			floatNumber, err := strconv.ParseFloat(token.Value, 64)
 			if err != nil {
@@ -161,14 +101,11 @@ func calculateEquation(tokens []Token) (float64, error) {
 			}
 
 			stack = append(stack, floatNumber)
-		} else {
-			if len(stack) < 2 {
-				return 0, fmt.Errorf("Невозможно посчитать значение, стек чисел меньше 2 при применении оператора")
+		} else if t := token.Type; t == Operator || t == Function {
+			err := calculateLocalResult(&stack, token)
+			if err != nil {
+				return 0, err
 			}
-			a, b := stack[len(stack)-2], stack[len(stack)-1]
-			stack = stack[:(len(stack) - 2)]
-			localResult := calculateLocalResult(a, b, token.Value)
-			stack = append(stack, localResult)
 		}
 	}
 
@@ -179,20 +116,59 @@ func calculateEquation(tokens []Token) (float64, error) {
 	return stack[0], nil
 }
 
-func calculateLocalResult(a, b float64, t string) float64 {
-	switch t {
+func calculateLocalResult(stack *[]float64, t Token) error {
+	switch t.Type {
 	// Операторы
-	case "+":
-		return a + b
-	case "-":
-		return a - b
-	case "/":
-		return a / b
-	case "*":
-		return a * b
+	case Operator:
+
+		if len(*stack) < 2 {
+			return fmt.Errorf("Невозможно посчитать значение, стек чисел меньше 2 при применении оператора")
+		}
+
+		var result float64 = 0
+		a, b := (*stack)[len(*stack)-2], (*stack)[len(*stack)-1]
+		*stack = (*stack)[:(len(*stack) - 2)]
+		switch t.Value {
+		case "+":
+			result = a + b
+		case "-":
+			result = a - b
+		case "*":
+			result = a * b
+		case "/":
+			result = a / b
+		}
+		*stack = append(*stack, result)
+	case Function:
+		switch t.Value {
+		case "sin":
+
+			if len(*stack) < 1 {
+				return fmt.Errorf("В стеке нет чисел для подсчета синуса")
+			}
+
+			a := (*stack)[len(*stack)-1]
+			*stack = (*stack)[:(len(*stack) - 1)]
+
+			result := math.Sin(a)
+
+			*stack = append(*stack, result)
+		case "cos":
+
+			if len(*stack) < 1 {
+				return fmt.Errorf("В стеке нет чисел для подсчета косинуса")
+			}
+
+			a := (*stack)[len(*stack)-1]
+			*stack = (*stack)[:(len(*stack) - 1)]
+
+			result := math.Cos(a)
+
+			*stack = append(*stack, result)
+		}
 	}
 
-	return 0
+	return nil
 }
 
 func calculateArgument(parameterTokens *[]Token, funcName string) (float64, error) {
