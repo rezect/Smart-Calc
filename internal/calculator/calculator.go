06f4509f0735be36fa16d74e19ebf71d9bf6ca03
rezect <ilyapr1568@yandex.ru>
2026-02-02 09:32:25 +0300

    feat: add recursive algo to calculate equation

--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -2,16 +2,21 @@ package calculator
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
-var tokenTypeValue = map[TokenType]int{
-	PLUS:     1,
-	MINUS:    1,
-	DIVIDE:   2,
-	MULTIPLY: 2,
-	LPAREN:   0,
-	RPAREN:   0,
+var tokenTypePriority = map[string]int{
+	"+": 1,
+	"-": 1,
+	"/": 2,
+	"*": 2,
+}
+
+var functionArgs = map[string]int{
+	"sin": 1,
+	"cos": 1,
+	"log": 2,
 }
 
 func HandleEquation(equationString string) error {
@@ -21,46 +26,95 @@ func HandleEquation(equationString string) error {
 		panic(err)
 	}
 
-	// Преобразуем в ОПН
-	tokensRPN, err := tokensToRPN(tokens)
-	if err != nil {
-		panic(err)
-	}
-
 	// Считаем значение выражения
-	result, err := calculateEquation(tokensRPN)
+	result, err := calculateEquation(tokens)
 	fmt.Printf("Результат: %v", result)
 
 	return nil
 }
 
+//5*8*cos(2+9)+(7*5+8-9*sin(5*5)+5)
 func tokensToRPN(tokens []Token) ([]Token, error) {
 	var operators = []Token{}
 	var rpn = []Token{}
 
-	for _, token := range tokens {
-		switch token.Type {
-		case NUMBER:
+	for i := 0; i < len(tokens); i++ {
+		token := tokens[i]
+		if token.Type == Number {
 			rpn = append(rpn, token)
-		default:
+		} else if token.Type == Function {
+			// Убеждаемся, что следующий токен существует и он - Lparen
+			if i >= len(tokens)-1 {
+				return nil, fmt.Errorf("После функции должны быть скобки")
+			} else if tokens[i+1].Type != Lparen {
+				return nil, fmt.Errorf("После функции должны быть скобки")
+			}
+
+			// Считываем токены до соотв. правой скобки
+			// TODO: более качественная обработка ошибок
+			var parameterTokens = []Token{}
+			var parameters = []float64{}
+
+			parenCounter := 0
+			funcToken := token
+			var funcResult float64 = 0
+
+			for j := i + 2; j < len(tokens); j++ {
+				token = tokens[j]
+				if token.Type == Comma {
+					argResult, err := calculateArgument(&parameterTokens, funcToken.Value)
+					if err != nil {
+						return nil, err
+					}
+
+					parameters = append(parameters, argResult)
+				} else {
+					if token.Type == Lparen {
+						parenCounter++
+					} else if token.Type == Rparen {
+						if parenCounter == 0 {
+							argResult, err := calculateArgument(&parameterTokens, funcToken.Value)
+							if err != nil {
+								return nil, err
+							}
+							
+							parameters = append(parameters, argResult)
+
+							switch funcToken.Value {
+							case "sin":
+								if len(parameters) != 1 {
+									return nil, fmt.Errorf("У синуса может быть только один параметр")
+								}
+								funcResult = math.Sin(parameters[0])
+							case "cos":
+								if len(parameters) != 1 {
+									return nil, fmt.Errorf("У косинуса может быть только один параметр")
+								}
+								funcResult = math.Cos(parameters[0])
+							}
+
+							i = j
+							break
+						} else {
+							parenCounter--
+						}
+					}
+					parameterTokens = append(parameterTokens, token)
+				}
+			}
+
+			rpn = append(rpn, Token{Number, strconv.FormatFloat(funcResult, 'f', -1, 64), 0})
+		} else if token.Type == Operator {
 			if len(operators) == 0 {
 				operators = append(operators, token)
 			} else {
-				lastOperatorType := operators[len(operators)-1].Type
-				if lastOperatorType == LPAREN || token.Type == LPAREN {
-					operators = append(operators, token)
-				} else if token.Type == RPAREN {
-					for operators[len(operators)-1].Type != LPAREN {
-						rpn = append(rpn, operators[len(operators)-1])
-						operators = operators[:(len(operators) - 1)]
-					}
-					operators = operators[:len(operators)-1]
-				} else if tokenTypeValue[lastOperatorType] < tokenTypeValue[token.Type] {
+				lastOperator := operators[len(operators)-1]
+				if lastOperator.Type == Lparen || tokenTypePriority[lastOperator.Value] < tokenTypePriority[token.Value] {
 					operators = append(operators, token)
 				} else {
 					for len(operators) > 0 {
-						lastOperatorType = operators[len(operators)-1].Type
-						if tokenTypeValue[lastOperatorType] < tokenTypeValue[token.Type] {
+						lastOperator = operators[len(operators)-1]
+						if tokenTypePriority[lastOperator.Value] < tokenTypePriority[token.Value] {
 							break
 						}
 						rpn = append(rpn, operators[len(operators)-1])
@@ -69,6 +123,20 @@ func tokensToRPN(tokens []Token) ([]Token, error) {
 					operators = append(operators, token)
 				}
 			}
+		} else if token.Type == Lparen {
+			operators = append(operators, token)
+		} else if token.Type == Rparen {
+			if len(operators) == 0 {
+				return nil, fmt.Errorf("Неправильное расположение закрывающей скобки")
+			}
+			for operators[len(operators)-1].Type != Lparen {
+				rpn = append(rpn, operators[len(operators)-1])
+				operators = operators[:(len(operators) - 1)]
+				if len(operators) == 0 {
+					return nil, fmt.Errorf("Неправильное расположение закрывающей скобки")
+				}
+			}
+			operators = operators[:len(operators)-1]
 		}
 	}
 	for len(operators) > 0 {
@@ -80,9 +148,13 @@ func tokensToRPN(tokens []Token) ([]Token, error) {
 
 func calculateEquation(tokens []Token) (float64, error) {
 	var stack = []float64{}
+	tokensRPN, err := tokensToRPN(tokens)
+	if err != nil {
+		return 0, err
+	}
 
-	for _, token := range tokens {
-		if token.Type == NUMBER {
+	for _, token := range tokensRPN {
+		if token.Type == Number {
 			floatNumber, err := strconv.ParseFloat(token.Value, 64)
 			if err != nil {
 				return 0, err
@@ -94,8 +166,8 @@ func calculateEquation(tokens []Token) (float64, error) {
 				return 0, fmt.Errorf("Невозможно посчитать значение, стек чисел меньше 2 при применении оператора")
 			}
 			a, b := stack[len(stack)-2], stack[len(stack)-1]
-			stack = stack[:(len(stack)-2)]
-			localResult := calculateLocalResult(a, b, token.Type)
+			stack = stack[:(len(stack) - 2)]
+			localResult := calculateLocalResult(a, b, token.Value)
 			stack = append(stack, localResult)
 		}
 	}
@@ -107,17 +179,32 @@ func calculateEquation(tokens []Token) (float64, error) {
 	return stack[0], nil
 }
 
-func calculateLocalResult(a, b float64, t TokenType) float64 {
-	var result float64 = 0
+func calculateLocalResult(a, b float64, t string) float64 {
 	switch t {
-	case PLUS:
-		result = a + b
-	case MINUS:
-		result = a - b
-	case MULTIPLY:
-		result = a * b
-	case DIVIDE:
-		result = a / b
+	// Операторы
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "/":
+		return a / b
+	case "*":
+		return a * b
 	}
-	return result
+
+	return 0
+}
+
+func calculateArgument(parameterTokens *[]Token, funcName string) (float64, error) {
+	if len(*parameterTokens) == 0 {
+		return 0, fmt.Errorf("Получен пустой аргумент функции %s", funcName)
+	}
+
+	// Считаем аргумент функции
+	result, err := calculateEquation(*parameterTokens)
+	if err != nil {
+		return 0, err
+	}
+
+	return result, nil
 }
--- internal/calculator/tokenizer.go
+++ internal/calculator/tokenizer.go
@@ -3,33 +3,47 @@ package calculator
 import (
 	"fmt"
 	"strings"
+	"unicode"
 	"unicode/utf8"
 )
 
 type TokenType int
 
 const (
-	NUMBER TokenType = iota
-	PLUS
-	MINUS
-	MULTIPLY
-	DIVIDE
-	LPAREN
-	RPAREN
-	EOF
+	Number TokenType = iota
+	Operator
+	Function
+	Lparen
+	Rparen
+	Comma
 )
 
-var tokenDict = map[rune]TokenType{
-	'+': PLUS,
-	'-': MINUS,
-	'/': DIVIDE,
-	'*': MULTIPLY,
-	'(': LPAREN,
-	')': RPAREN,
+var operatorsDict = map[rune]TokenType{
+	'+': Operator,
+	'-': Operator,
+	'/': Operator,
+	'*': Operator,
+	'(': Lparen,
+	')': Rparen,
+	',': Comma,
+}
+
+var validFunctions = []string{
+	"sin",
+	"cos",
+}
+
+func isFunctionValid(fName string) bool {
+	for _, validFuncName := range validFunctions {
+		if validFuncName == fName {
+			return true
+		}
+	}
+	return false
 }
 
 type Token struct {
-	Type     TokenType
+	Type     TokenType	// Number, Operator, Function, Lparen, Rparen
 	Value    string
 	Position int
 }
@@ -42,33 +56,48 @@ func tokenizeString(s string) ([]Token, error) {
 	stringNormalization(&s)
 
 	tokenList := make([]Token, 0)
-	var curNumber = Token{NUMBER, "", 0}
+	var curToken = Token{Number, "", 0}
+
+	var allOperators strings.Builder
+	for k := range operatorsDict {
+		allOperators.WriteString(string(k))
+	}
 	
 	for i, ch := range s {
-		switch ch {
-		case '+', '-', '/', '*', '(', ')', ' ':
-			if (curNumber.Value != "") {
-				tokenList = append(tokenList, curNumber)
-				curNumber.Value = ""
-				curNumber.Position = 0
+		if strings.ContainsRune(allOperators.String(), ch) || ch == ' ' {
+			if (curToken.Value != "") {
+				err := addToken(&curToken, &tokenList)
+				if err != nil {
+					return nil, err
+				}
 			}
 			if (ch != ' ') {
-				tokenList = append(tokenList, Token{tokenDict[ch], string(ch), i})
+				tokenList = append(tokenList, Token{operatorsDict[ch], string(ch), i})
 			}
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			if curNumber.Value == "" {
-				curNumber.Position = i
+		} else if unicode.IsDigit(ch) || ch == '.' {
+			if ch == '.' && curToken.Value == "" {
+				return nil, fmt.Errorf("Неправильно расположенный разделитель нецелого числа")
 			}
-			curNumber.Value += string(ch)
-		default:
+			if curToken.Value == "" {
+				curToken.Type = Number
+				curToken.Position = i
+			}
+
+			curToken.Value += string(ch)
+		} else if unicode.IsLetter(ch) {
+			if curToken.Value == "" {
+				curToken.Position = i
+			}
+
+			curToken.Type = Function
+			curToken.Value += string(ch)
+		} else {
 			return nil, fmt.Errorf("Встречен неизвестный символ на позиции %v", i)
 		}
 	}
 
-	if curNumber.Value != "" {
-		tokenList = append(tokenList, curNumber)
-		curNumber.Value = ""
-		curNumber.Position = 0
+	if curToken.Value != "" {
+		tokenList = append(tokenList, curToken)
 	}
 
 	return tokenList, nil
@@ -76,4 +105,18 @@ func tokenizeString(s string) ([]Token, error) {
 
 func stringNormalization(s *string) {
 	*s = strings.Trim(*s, "\n\r")
+}
+
+func addToken(token *Token, tokenList *[]Token) error {
+	if token.Type == Function {
+		if !isFunctionValid(token.Value) {
+			return fmt.Errorf("Неизвестное название функции: %v", token.Value)
+		}
+	}
+	*tokenList = append(*tokenList, *token)
+	token.Type = Number
+	token.Value = ""
+	token.Position = 0
+
+	return nil
 }
