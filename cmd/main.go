ce326731ba849527cd538087d367384fbd839e11
rezect <ilyapr1568@yandex.ru>
2026-02-02 17:38:35 +0300

    fix: redirect input control to cli, output now also in cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-
-	"Smart-Calc/internal/calculator"
+	"Smart-Calc/internal/cli"
 )
 
 func main() {
-	if (len(os.Args) > 1) {
-		handleArgs()
-	} else {
-		handleStdin()
-	}
-}
-
-func handleArgs() {
-	equations := os.Args[1:]
-	
-	for _, equation := range equations {
-		_, err := calculator.HandleEquation(equation)
-		if err != nil {
-			fmt.Printf("[ERROR] Incorrect equation: %s\n", equation)
-		}
-	}
-}
-
-func handleStdin() {
-	scanner := bufio.NewScanner(os.Stdin)
-	
-	for scanner.Scan() {
-		equation := scanner.Text()
-		_, err := calculator.HandleEquation(equation)
-		if err != nil {
-			fmt.Printf("[ERROR] Incorrect equation: %s\n", equation)
-		}
-	}
+	cli.Cli()
 }
--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -2,7 +2,6 @@ package calculator
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func HandleEquation(equationString string) (float64, error) {
@@ -10,24 +9,20 @@ func HandleEquation(equationString string) (float64, error) {
 		return 0, fmt.Errorf("Пустая строка")
 	}
 
-	// Разбиваем задачи на токены
 	tokens, err := tokenizeString(equationString)
 	if err != nil {
 		return 0, err
 	}
 
-	// Преобразуем в RPN
 	tokensRPN, err := tokensToRPN(tokens)
 	if err != nil {
 		return 0, err
 	}
 
-	// Считаем значение выражения
 	result, err := calculateEquation(tokensRPN)
 	if err != nil {
 		return 0, err
 	}
-	fmt.Println("Результат: ", strconv.FormatFloat(result, 'f', -1, 64))
 
 	return result, nil
 }
